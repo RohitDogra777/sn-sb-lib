34a6a6bae2c1711152c030110914d8dd5a74f28e
Rohit Dogra <rohit.kumardogra@servicenow.com>
2024-05-21 11:34:08 +0530

    changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function loadWebComponents () {
    * Load Avatar Component if Used in DOM Test
    * */
   if (document.querySelector("ods-avatar")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.5/chunks/Avatar-BkeLH5Vq.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.7/chunks/Avatar-BkeLH5Vq.js').then(function () {
       console.log("Avatar Component Chunk Loaded");
     });
   }
@@ -15,7 +15,7 @@ function loadWebComponents () {
    * Load Heading Component If used in DOM
    */
   if (document.querySelector("ods-heading")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.5/chunks/Heading-Dn016aUz.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.7/chunks/Heading-Dn016aUz.js').then(function () {
       console.log("Heading Component Chunk Loaded");
     });
   }
@@ -24,7 +24,7 @@ function loadWebComponents () {
    * Load Text Component If used in DOM
    */
   if (document.querySelector("ods-text")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.5/chunks/Text-DKPqFkRo.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.7/chunks/Text-DKPqFkRo.js').then(function () {
       console.log("Text Component Chunk Loaded");
     });
   }
@@ -33,7 +33,7 @@ function loadWebComponents () {
    * Load Icon Component If used in DOM
    */
   if (document.querySelector("ods-icon")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.5/chunks/Icon-YiZtAaer.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.7/chunks/Icon-YiZtAaer.js').then(function () {
       console.log("Icon Component Chunk Loaded");
     });
   }
@@ -41,7 +41,7 @@ function loadWebComponents () {
    * Load Link Component If used in DOM
    */
   if (document.querySelector("ods-link")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.5/chunks/Link-CE6NU8R5.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.7/chunks/Link-CE6NU8R5.js').then(function () {
       console.log("Link Component Chunk Loaded");
     });
   }
@@ -50,7 +50,7 @@ function loadWebComponents () {
    * Load Card Component If used in DOM
    */
   if (document.querySelector("ods-card")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.5/chunks/Card-uUUl9Q5m.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v2.0.7/chunks/Card-uUUl9Q5m.js').then(function () {
       console.log("Card Component Chunk Loaded");
     });
   }
@@ -63,6 +63,6 @@ var intervalId = setInterval(function(){
       console.log(intervalId);
         if(document.readyState === 'complete'){
            loadWebComponents();
-           //clearInterval(intervalId);
+           clearInterval(intervalId);
         }
 },500);
