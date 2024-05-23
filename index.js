7c9f5c327cb7b2cdb3e3543d8487ab1249970389
Rohit Dogra <rohit.kumardogra@servicenow.com>
2024-05-23 21:27:42 +0530

    changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function loadWebComponents() {
    * Load Avatar Component if Used in DOM
    * */
   if (document.querySelector("ods-avatar")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.0/chunks/Avatar-LNIklOIX.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.1/chunks/Avatar-LNIklOIX.js').then(function () {
       console.log("Avatar Component Chunk Loaded");
     });
   }
@@ -15,7 +15,7 @@ function loadWebComponents() {
    * Load Heading Component If used in DOM
    */
   if (document.querySelector("ods-heading")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.0/chunks/Heading-wKT0e24g.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.1/chunks/Heading-wKT0e24g.js').then(function () {
       console.log("Heading Component Chunk Loaded");
     });
   }
@@ -24,7 +24,7 @@ function loadWebComponents() {
    * Load Text Component If used in DOM
    */
   if (document.querySelector("ods-text")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.0/chunks/Text-C6-S6tLh.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.1/chunks/Text-C6-S6tLh.js').then(function () {
       console.log("Text Component Chunk Loaded");
     });
   }
@@ -33,7 +33,7 @@ function loadWebComponents() {
    * Load Icon Component If used in DOM
    */
   if (document.querySelector("ods-icon")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.0/chunks/Icon-CjWNPvlv.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.1/chunks/Icon-CjWNPvlv.js').then(function () {
       console.log("Icon Component Chunk Loaded");
     });
   }
@@ -41,7 +41,7 @@ function loadWebComponents() {
    * Load Button Component If used in DOM
    */
   if (document.querySelector("ods-button")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.0/chunks/Button-2nPJ1Pk3.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.1/chunks/Button-2nPJ1Pk3.js').then(function () {
       console.log("Button Component Chunk Loaded");
     });
   }
@@ -50,7 +50,7 @@ function loadWebComponents() {
    * Load Container Component If used in DOM
    */
   if (document.querySelector("ods-container")) {
-    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.0/chunks/Container-C_Co7DJ7.js').then(function () {
+    import('https://cdn.jsdelivr.net/gh/RohitDogra777/sn-sb-lib@v3.0.1/chunks/Container-C_Co7DJ7.js').then(function () {
       console.log("Container Component Chunk Loaded");
     });
   }
@@ -62,6 +62,8 @@ function loadWebComponents() {
 var intervalId = setInterval(function () {
   if (document.readyState === 'complete') {
     loadWebComponents();
-    clearInterval(intervalId);
+    setTimeout(function(){
+      clearInterval(intervalId);
+    },3000);
   }
-}, 100);
+}, 300);
